6a61f4c3721943e816b65c4e7250ec3b5feea7a1
rimiyeyo <bhr0212@naver.com>
2023-11-05 12:52:48 +0900

    frame

--- src/main.py
+++ src/main.py
@@ -1,49 +1,63 @@
+import os
+import openai
+from dotenv import load_dotenv
 from fastapi import FastAPI
-from uuid import UUID, uuid4
-from typing import List, Union, Optional, Dict, Any
-from pydantic import BaseModel, Field
-import uvicorn
+from pydantic import BaseModel
+from youtube_transcript_api import YouTubeTranscriptApi
 
-# FastAPI 객체 생성
 app = FastAPI()
 
-# "/"로 접근하면 return을 보여줌
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+load_dotenv()
+openai.api_key=os.getenv("OPENAI_API_KEY")
 
-class Content(BaseModel):
-    id : UUID = Field(default_factory=uuid4)
-    title : str
-    txt : List[str]
+class UrlRequest(BaseModel):
+    urlink: str # 사용자가 youtube link입력함.
 
-class Youtube_Info(BaseModel):
-    id : UUID = Field(default_factory=uuid4)
-    name : str
-    products : List[Content] = Field(default_factory=list)
-    creadted_at : datetime = Field(default_factory=datetime.now)
-    updated_at : datetime = Field(default_factory=datetime.now)
+@app.post("/load")
+def get_youtube_text(req: UrlRequest):
+    url_value = req.urlink.split('v=')[-1]
+    srt = YouTubeTranscriptApi.get_transcript(url_value, languages=['ko'])
+    text = ''
+    for i in srt:
+        words = i['text'].split(' ')
+        lst = ['니다', '세요', '고요', '구요', '데요', '까요', '에요', '어요', '내요','되요', '든요', '아요', '해요', '게요', '네요', '뭡니까']
+        
+        # 문장부호 붙이기
+        sen = ''
+        for word in words:
+            for j in lst:
+                if j == word[-len(j):]:
+                    word += '.'
+            sen += word + ' '
+        text += sen
+    return text
 
-class MessageResponse(BaseModel):
+class ChatRequest(BaseModel):
     message: str
-    
-youtube_texts = [] #############################################
-@app.get()("/info", description="한 youtube의 텍스트들을 가져옵니다.")
-async def get_youtube_texts() -> List[Content]:
-    # youtube에 있는 크롤링해서 text 추출 code 삽입
-    return youtube_texts
 
-@app.get("/info/{youtube_id}", description="youtub 정보를 가져옵니다.")
-async def get_youtube(youtube_id: UUID) -> Union[Youtube_Info, MessageResponse]:
-    youtube_text = get_youtube_texts() #### 코드 완성 안함.
-    if not youtube_text:
-        return MessageResponse(message = "추출할 텍스트가 없습니다.")
-    return youtube_text
+@app.post("/chat")
+def chat(req: ChatRequest):
+    response = openai.ChatCompletion.create(
+        model = "gpt-4",
+        message = [
+            {"role": "system", "content": "your teacher"},
+            {"role": "user", "content": req.message},
+        ]
+    )
+    return {"message": response.choices[0].message.content}
 
-# youtube text받아옴---> text를 기반으로 질문도 생성해야하고, 질문을 qa에 넣으면 답변 생성
-@app.post("/info", description="youtube text를 받아와서 질문 생성 및 답변 생성")
-async def make_question_answer(texts: List[get_youtube_texts]): #model, # config
-    # 1. youtube 텍스트 가져오기
-    # 2. 모델에 text넣고 질문 생성
-    # 3. 정답확인 (result)
-    return
+# 1. youtube text에 대한 질문 생성
+    # 키워드추출로 질문생성할껀가..... 이게 좀 많이 어렵네.. 
+# 2. 질문에 대한 답변
+    # 웹 서치 vector DB 붙여서 만들기 ----> 관련내용 계속 공부중.....
+# 3. 답변에서 또 질문
+# 4. 답변 4~5턴 종료 후
+
+# ------------------------------ 추후에 해볼 것
+# 비슷한 주제의 동영상 추천받기
+# 영상시청 후 ---> 영상 내용 summary. 
+# 해당 영상에 대한 질문 답변
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
