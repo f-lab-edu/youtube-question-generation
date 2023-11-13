e7a6485a8d36552289e823ee3c7bdd1fe7848eec
rimiyeyo <bhr0212@naver.com>
2023-11-13 20:22:14 +0900

    modify1113

--- src/main.py
+++ src/main.py
@@ -1,43 +1,50 @@
 import os
 import openai
+import re
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
+from pydantic_settings import BaseSettings
 from youtube_transcript_api import YouTubeTranscriptApi
 
 app = FastAPI()
 
+class Settings(BaseSettings):
+    openai_api_key: str
+
+# .env 파일에서 환경 변수 로드
 load_dotenv()
-openai.api_key=os.getenv("OPENAI_API_KEY")
+# Settings 클래스 인스턴스 생성
+settings = Settings()
+openai.api_key = settings.openai_api_key
 
 class UrlRequest(BaseModel):
     urlink: str # 사용자가 youtube link입력함.
 
 @app.post("/load")
 def get_youtube_text(req: UrlRequest):
-    url_value = req.urlink.split('v=')[-1]
+    
+    reg_exp = re.compile(r'^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*')
+    match = reg_exp.match(req)
+    url_value = match.group(7) if match and len(match.group(7)) == 11 else False
+    
     srt = YouTubeTranscriptApi.get_transcript(url_value, languages=['ko'])
+    
     text = ''
+    lst = ['니다', '세요', '고요', '구요', '데요', '까요', '에요', '어요', '내요', '되요', '든요', '아요', '해요', '게요', '네요', '뭡니까']
+
     for i in srt:
         words = i['text'].split(' ')
-        lst = ['니다', '세요', '고요', '구요', '데요', '까요', '에요', '어요', '내요','되요', '든요', '아요', '해요', '게요', '네요', '뭡니까']
-        
-        # 문장부호 붙이기
-        sen = ''
-        for word in words:
-            for j in lst:
-                if j == word[-len(j):]:
-                    word += '.'
-            sen += word + ' '
-        text += sen
+        sen = ' '.join([word + '.' if word.endswith(j) else word for word in words for j in lst])
+        text += sen + ' '
     return text
 
 class ChatRequest(BaseModel):
     message: str
 
 @app.post("/chat")
-def chat(req: ChatRequest):
-    response = openai.ChatCompletion.create(
+async def chat(req: ChatRequest):
+    response = await openai.ChatCompletion.acreate(
         model = "gpt-4",
         message = [
             {"role": "system", "content": "your teacher"},
