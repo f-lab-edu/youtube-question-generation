46d268f461aee0b2619faba5010d5181081dd6ab
rimiyeyo <bhr0212@naver.com>
2023-11-26 01:59:24 +0900

    feat: LangChain QA init_model

--- src/main.py
+++ src/main.py
@@ -1,12 +1,20 @@
-import os
 import openai
-import re
+import yt_dlp as yt
+import re, time, math
+from pytube import YouTube
 from fastapi import FastAPI
 from pydantic import BaseModel
+from emb import document_split
+from model import transcribe_file
+from langchain.vectorstores import Chroma
+from langchain.docstore.document import Document
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from youtube_transcript_api import YouTubeTranscriptApi
 
 app = FastAPI()
+
+AUDIO_FOLDER = './audio'
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
@@ -17,52 +25,62 @@ settings = Settings(_env_file='.env', _env_file_encoding='utf-8')
 openai.api_key = settings.openai_api_key
 
 class UrlRequest(BaseModel):
-    urlink: str # 사용자가 youtube link입력함.
+    # 사용자가 youtube link 입력함.
+    urlink: str 
 
-@app.post("/load") # 반환타입 적기
-def get_youtube_text(req: UrlRequest) -> str:
+class QuesRequest(BaseModel):
+    # 질의를 합니다.
+    query: str
+
+@app.post("/youtube_key")
+def get_youtube_key(req: UrlRequest) -> str:
     
     # https://stackoverflow.com/questions/3452546/how-do-i-get-the-youtube-video-id-from-a-url
     url_parts = re.split(r'(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)', req)
     url_value = url_parts[2].split(r'[^0-9a-z_\-]', 1)[0] if url_parts[2] is not None else url_parts[0]
-    
-    srt = YouTubeTranscriptApi.get_transcript(url_value, languages=['ko'])
-    
-    text = ''
-    # 정규 표현식 패턴으로 종결어미를 찾아 문장 끝에 '.' 추가
-    pattern = re.compile(r'(니다|세요|고요|구요|데요|까요|에요|어요|내요|되요|든요|아요|해요|게요|네요|뭡니까)(?=\s|$)')
-    
-    for i in srt:
-        # 정규 표현식을 사용하여 종결어미에 '.' 추가
-        modified_text = pattern.sub(r'\1.', i['text'])
-        text += modified_text + ' '
-    return text
+    return url_value
 
-class ChatRequest(BaseModel):
-    message: str
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': f'{AUDIO_FOLDER}/%(id)s.%(ext)s',
+}
+
+@app.post("/audioload")
+def get_youtube_audio(req: UrlRequest):
+    # audio 파일이 생성되는데 return type 어떻게 해야할지 모르겠음.
+    with yt.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([req])
+
+def audio_to_text(req: UrlRequest) -> Chroma:
+    youtube_urlkey = get_youtube_key(req = req)
+    audio_filename = f'./{youtube_urlkey}.webm'
+
+    start = time.perf_counter()
+    transcription = transcribe_file(audio_filename) 
+    runtime = time.perf_counter()-start
+    rounded_runtime = math.ceil(runtime)
+    print("Runtime: ", rounded_runtime, " seconds")
+
+    texts = [Document(page_content=transcription)]
+    db = document_split(texts)
+    return db
+
+def make_qa_chain(req: UrlRequest) -> RetrievalQA:
+    db = audio_to_text(req)
+    llm = ChatOpenAI(model_name="gpt-4", temperature=0, api_key="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")
+    return RetrievalQA.from_chain_type(
+        llm,
+        retriever=db.as_retriever(search_type="mmr", search_kwargs={'fetch_k': 3}),
+        return_source_documents=True
+    )
 
 @app.post("/chat")
-async def chat(req: ChatRequest):
-    response = await openai.ChatCompletion.acreate(
-        model = "gpt-4",
-        message = [
-            {"role": "system", "content": "your teacher"},
-            {"role": "user", "content": req.message},
-        ]
-    )
-    return ChatRequest(message=response.choices[0].message.content)
-
-# 1. youtube text에 대한 질문 생성
-    # 키워드추출로 질문생성할껀가..... 이게 좀 많이 어렵네.. 
-# 2. 질문에 대한 답변
-    # 웹 서치 vector DB 붙여서 만들기 ----> 관련내용 계속 공부중.....
-# 3. 답변에서 또 질문
-# 4. 답변 4~5턴 종료 후
-
-# ------------------------------ 추후에 해볼 것
-# 비슷한 주제의 동영상 추천받기
-# 영상시청 후 ---> 영상 내용 summary. 
-# 해당 영상에 대한 질문 답변
+async def ask_question(req: UrlRequest, q: QuesRequest):
+    qa_chain = make_qa_chain(req)
+    result = qa_chain({"query": q})
+    print(f"Q: {result['query'].strip()}")
+    print(f"A: {result['result'].strip()}\n")
+    print('\n')
 
 if __name__ == "__main__":
     import uvicorn
---
+++ src/model.py
@@ -0,0 +1,40 @@
+import torch
+import logging
+from pydantic import BaseModel
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+model_id = "distil-whisper/distil-large-v2"
+
+class TextResponse(BaseModel):
+    # speech -> text
+    transcription: str 
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id,
+    torch_dtype=torch_dtype,
+    low_cpu_mem_usage=True,
+    use_safetensors=True,
+    use_flash_attention_2=True # Remove is using a CPU
+)
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+pipeline = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    max_new_tokens=128,
+    chunk_length_s=15,
+    batch_size=16,
+    torch_dtype=torch_dtype,
+    device=device,
+)
+
+def transcribe_file(filename):
+    logging.info("Transcribing New file: {filename}")
+    transcription = pipeline(filename, return_timestamps=True)
+    return TextResponse(transcription=transcription['text'])
