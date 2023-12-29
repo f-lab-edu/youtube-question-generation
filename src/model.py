c7db3b1dc71667ade62593d667eb84964f0cb55d
Sungmin Han (F-Lab) <101314896+FlabKenneth@users.noreply.github.com>
2023-12-30 00:40:06 +0900

    feature/setup-pytest (#19)
    * feat: wrap pipeline with cached function for the testable design
    
    * chore: add pytest in requirements.txt
    
    * test: add test specs
    
    * docs: add installation and testing guide at README.md
    
    ---------
    
    Co-authored-by: KennethanCeyer <kenneth@pigno.se>

--- src/model.py
+++ src/model.py
@@ -1,8 +1,10 @@
 import logging
+from functools import lru_cache
 
 import torch
 from pydantic import BaseModel
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from transformers.pipelines import Pipeline
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
@@ -14,31 +16,34 @@ class TextResponse(BaseModel):
     transcription: str
 
 
-model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id,
-    torch_dtype=torch_dtype,
-    low_cpu_mem_usage=True,
-    use_safetensors=True,
-    use_flash_attention_2=True,  # Remove is using a CPU
-)
-model.to(device)
+@lru_cache(maxsize=None)
+# Singleton Pattern
+def get_pipeline() -> Pipeline:
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id,
+        torch_dtype=torch_dtype,
+        low_cpu_mem_usage=True,
+        use_safetensors=True,
+        use_flash_attention_2=True,  # Remove is using a CPU
+    )
+    model.to(device)
+    processor = AutoProcessor.from_pretrained(model_id)
+    return pipeline(
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        max_new_tokens=128,
+        chunk_length_s=15,
+        batch_size=16,
+        torch_dtype=torch_dtype,
+        device=device,
+    )
 
-processor = AutoProcessor.from_pretrained(model_id)
 
-pipeline = pipeline(
-    "automatic-speech-recognition",
-    model=model,
-    tokenizer=processor.tokenizer,
-    feature_extractor=processor.feature_extractor,
-    max_new_tokens=128,
-    chunk_length_s=15,
-    batch_size=16,
-    torch_dtype=torch_dtype,
-    device=device,
-)
-
-
-def transcribe_file(filename):
+def transcribe_file(filename: str) -> TextResponse:
     logging.info("Transcribing New file: {filename}")
+
+    pipeline = get_pipeline()
     transcription = pipeline(filename, return_timestamps=True)
     return TextResponse(transcription=transcription["text"])
---
+++ src/tests/test_main.py
@@ -0,0 +1,62 @@
+from typing import Any
+from unittest.mock import patch
+
+from main import UrlRequest, get_youtube_audio, get_youtube_key
+
+
+def test_get_youtube_key_with_kurzgesagt():
+    # Given
+    pass
+
+    # When
+    url_youtube_kurzgesagt = "https://www.youtube.com/watch?v=cFslUSyfZPc"
+    input_req = UrlRequest(urlink=url_youtube_kurzgesagt)
+    output = get_youtube_key(input_req)
+
+    # Then
+    assert output == "cFslUSyfZPc"
+
+
+def test_get_youtube_short_key_with_kurzgesagt():
+    # Given
+    pass
+
+    # When
+    url_youtube_short_kurzgesagt = "https://youtu.be/cFslUSyfZPc"
+    input_req = UrlRequest(urlink=url_youtube_short_kurzgesagt)
+    output = get_youtube_key(input_req)
+
+    # Then
+    assert output == "cFslUSyfZPc"
+
+
+def test_get_youtube_embed_key_with_kurzgesagt():
+    # Given
+    pass
+
+    # When
+    url_youtube_embed_kurzgesagt = "https://www.youtube.com/embed/cFslUSyfZPc?si=fqKkzm2GgVUen-VM"
+    input_req = UrlRequest(urlink=url_youtube_embed_kurzgesagt)
+    output = get_youtube_key(input_req)
+
+    # Then
+    assert output == "cFslUSyfZPc"
+
+
+@patch("yt_dlp.YoutubeDL")
+def test_get_youtube_audio_should_run_ydl_download(mock_youtube_dl: Any):
+    # Given
+    mocked_download = mock_youtube_dl.return_value.__enter__.return_value.download
+
+    # When
+    input_req = UrlRequest(urlink="https://youtu.be/cFslUSyfZPc")
+    get_youtube_audio(input_req)
+
+    # Then
+    mock_youtube_dl.assert_called_once_with(
+        {
+            "format": "bestaudio/best",
+            "outtmpl": "./audio/%(id)s.%(ext)s",
+        }
+    )
+    mocked_download.assert_called_once_with([input_req])
